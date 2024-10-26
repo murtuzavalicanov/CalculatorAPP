3ac52ab39a5486b14987f96c5daa545270e3e4a9
murtuzavalicanov <you@example.com>
2024-10-26 15:04:31 +0400

    my first repository

---
+++ CalculatorAPP/src/main/java/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class Calculator {
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double plus(double a, double b) {
+        return a + b;
+    }
+
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+
+    public static double percent(double a) {
+        return a / 100;
+    }
+}
